ffa48dc8101e8cab68212cb019b5da8c367e5d6e
Rodrigo Vasto <sabradios@live.com.mx>
2024-12-09 20:14:18 -0600

    feat: :fire: Update1.1

--- Django/graficas/views.py
+++ Django/graficas/views.py
@@ -9,22 +9,29 @@ def get_graph_data():
     return [
         {
             'etiquetas': ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo'],
-            'datos': [5, 15, 10, 20, 250]
+            'datos': [5, 15, 10, 20, 25]
         },
         {
             'etiquetas': ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo'],
-            'datos': [5, 15, 10, 30, 25]
+            'datos': [5, 15, 10, 20, 25]
         },
         {
             'etiquetas': ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo'],
             'datos': [268, 156, 250, 20, 25]
         }
     ]
+
 def graficas_view(request):
+    """
+    Render the graphs view with prepared data.
+    Args:
+        request: HTTP request object.
+    Returns:
+        Rendered HTML template with graph data.
+    """
     graph_data = get_graph_data()
-    print("Graph Data:", graph_data)  # Para verificar que los datos son correctos
-    return render(request, 'graficas/Grafica.html', {
+    return render (request, 'graficas/graficas.html', {
         'grafica1': graph_data[0],
         'grafica2': graph_data[1],
         'grafica3': graph_data[2],
-    })
+})
